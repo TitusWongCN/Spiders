51282c483f5a16c647f782cebc10fc94e6cb318c
TitusWongCN <wzhwno1@163.com>
2020-09-11 01:00:18 +0800

    auto appoint device phase I complete

--- AutoAppointDevice/main.py
+++ AutoAppointDevice/main.py
@@ -1,65 +1,27 @@
-# -*- coding=utf-8 -*-
-# python37
-import requests
+from selenium import webdriver
+import time
 
-url = r'http://121.192.177.40/lfsms/personbook/timeadd?insid=65546&f=person&c=lfsmspersonbooktimeadd'
+chrome = webdriver.Chrome(executable_path=r'D:\Myprograms\chromedriver.exe')
+chrome.get('http://open.xmu.edu.cn/Login?returnUrl=http%3A%2F%2Fopen.xmu.edu.cn%2Foauth2%2Fauthorize%3Fclient_id%3D1089%26response_type%3Dcode')
+chrome.find_element_by_id('login-username').send_keys('20520160154055')
+chrome.find_element_by_id('login-password').send_keys('******')
+chrome.find_element_by_id('LoginButton').click()
 
-headers = {
-'Accept': 'application/json, text/javascript, */*; q=0.01',
-'Accept-Encoding': 'gzip, deflate',
-'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-'Connection': 'keep-alive',
-'Content-Length': '1334',
-'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-'Host': '121.192.177.40',
-'Origin': 'http://121.192.177.40',
-'Referer': 'http://121.192.177.40/lfsms/personbook/timeadd?insid=65546&f=person&c=lfsmspersonbooktimeadd',
-'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36',
-'X-Requested-With': 'XMLHttpRequest',
-}
+chrome.get('http://121.192.177.40/lfsms/personbook/timeadd?insid=65546&f=person&c=lfsmspersonbooktimeadd')
 
-cookies = {
-    'CNZZDATA1277850985': '358178429-1598365423-%7C1598713171	',
-    'PHPSESSID': 'rp20oodbrgn518b62pan5mql31',
-    'UM_distinctid': '174260655b22bd-01a13496298562-67e153a-1fa400-174260655b3426	',
-    'YII_CSRF_TOKEN': 'QUxYTmE0dVFta2FBU2VsQ293blVEZW1tUUpHTUZmZDKl8TJdc',
-    'hidden': 'value',
-}
-cookie = ''
-for name, value in cookies.items():
-    cookie += '{0}={1};'.format(name, value)
-cookie += '{0}={1};'.format('hidden', 'value')
-headers['Cookie'] = cookies
+js = '$("#startTime").val("{{startTime}}");$("#endTime").val("{{endTime}}");$("#FC-Form").submit();'
+js = js.replace('{{startTime}}', '2020-09-13 08:45:00').replace('{{endTime}}', '2020-09-13 09:00:00')
 
-data = {
-'StartTime[DayDate]': '2020-08-30',
-'StartTime[HourDate]': '20',
-'StartTime[MinuteDate]': '15',
-'EndTime[DayDate]': '2020-08-30',
-'EndTime[HourDate]': '20',
-'EndTime[MinuteDate]': '30',
-'CAR_Tbookingrecord[BillingProFileId]': '2131967961',
-'CAR_Tbookinghassample[SampleName]': '123',
-'CAR_Tbookinghassample[SampleCount]': '456',
-'CAR_Tbookinghassample[SampleDeliveryMan]': '',
-'CAR_Tbookinghassample[SampleDeliveryTime]': '',
-'CAR_Tbookinghassample[SamplePretreating]': '0',
-'CAR_Tbookinghassample[SampleDetail]': '',
-'CAR_Tbookingrecord[Comment]': '',
-'CAR_TuserReport[subjectName]': '无',
-'CAR_TuserReport[serviceContent]': '样品测试',
-'CAR_TuserReport[serviceWay][]': '占用共享',
-'CAR_TuserReport[serviceWay][]': '技术共享',
-'CAR_TuserReport[subjectIncome][]': '国家自然科学基金',
-'CAR_TuserReport[subjectArea][]': '化学',
-'CAR_TuserReport[serviceType]': '内部用户',
-'CAR_TuserReport[serviceDirection]': '科学研究',
-'CAR_TuserReport[subjectContent]': '无',
-'CAR_TuserReport[applicantUnit]': '厦门大学化学化工学院',
-'CAR_TuserReport[record]': '无',
-}
+chrome.execute_script(js)
 
-result = requests.post(url, data=data)
-print(result)
-print(result.json())
+sample_name = chrome.find_element_by_id('CAR_Tbookinghassample_SampleName')
+while not sample_name:
+    sample_name = chrome.find_element_by_id('CAR_Tbookinghassample_SampleName')
+    time.sleep(1)
+sample_name.send_keys('1234')
+sample_count = chrome.find_element_by_id('CAR_Tbookinghassample_SampleCount')
+sample_count.send_keys('1234')
 
+chrome.execute_script('$("#formId").submit();')
+
+print('DONE')
